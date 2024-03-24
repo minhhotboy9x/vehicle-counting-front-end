cd3bb3a902f54dfe0b34fe2015b350275b09f47e
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2024-03-24 23:38:27 +0700

    env

--- src/components/VideoStream.js
+++ src/components/VideoStream.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoStream = () => {
     // Replace the URL with the actual endpoint of your Flask video_feed
-    const videoFeedUrl = "http://localhost:5000/video_feed";
+    const videoFeedUrl = "http://localhost:5000/streaming/1";
 
     return (
         <img src={videoFeedUrl} alt="Video Stream" style={{ width: 640, height: 480 }}  />
